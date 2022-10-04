fa50a556ba81d10f3fd73cb6ae4b6c1145bd5cb7
Vincent Carluer <vcarluer@gmail.com>
2022-10-04 17:41:50 +0200

    working get organizations

---
+++ README.md
@@ -0,0 +1,12 @@
+
+
+## Log in with az cli
+[Install Powershell](https://learn.microsoft.com/en-us/powershell/scripting/install/install-debian?view=powershell-7.2)
+[Install Powershell AZ module](https://learn.microsoft.com/en-us/powershell/azure/install-az-ps?view=azps-8.3.0)
+``` powershell
+Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
+```
+Connect in powershell
+``` powershell
+Connect-AzAccount
+```
--- main.py
+++ main.py
@@ -3,22 +3,68 @@
 
 from azure.identity import DefaultAzureCredential
 import cosmotech_api
+from cosmotech_api import Configuration
+from cosmotech_api import ApiClient
+from cosmotech_api.api import organization_api
 import logging
 import sys
+import yaml
 
 logger = logging.getLogger()
 fileHandler = logging.FileHandler("application.log")
 streamHandler = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
 streamHandler.setFormatter(formatter)
 fileHandler.setFormatter(formatter)
 logger.addHandler(streamHandler)
 logger.addHandler(fileHandler)
 logger.setLevel(logging.DEBUG)
 
+
+def get_apiclient(config):
+    host = config['platform']['url']
+    scope = config['platform']['scope']
+
+    logger.debug("logging in")
+    credential = DefaultAzureCredential()
+    logger.debug("Getting token")
+    token = credential.get_token(scope)
+
+    configuration = Configuration(
+        host=host,
+        discard_unknown_keys=True,
+        access_token=token.token
+    )
+
+    return ApiClient(configuration)
+
+
+def migrate_organizations(config, api_client):
+    try:
+        api_organization = organization_api.OrganizationApi(api_client)
+        organizations = api_organization.find_all_organizations()
+        logger.debug(organizations)
+    except cosmotech_api.ApiException as e:
+        logger.error(
+                "Exception when calling " +
+                "organization_api->find_all_datasets: " +
+                f"{e}"
+                )
+
+
+def get_config():
+    with open('config.yaml', 'r') as config_file:
+        return yaml.safe_load(config_file)
+
+
 def migrate():
     """Migrate a platform oid to RBAC security v2"""
     logging.info("Migration start")
+    config = get_config()
+    with get_apiclient(config) as api_client:
+        migrate_organizations(config, api_client)
 
 
 if __name__ == "__main__":
